d8cbcd58db237530e4fc7db2c999efd67fc5a56c
elitk <elitk2795@gmail.com>
2021-05-01 22:24:00 +0300

    remove cors header

--- src/utils/getSuggestions.js
+++ src/utils/getSuggestions.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { apiKey } from "../api/config";
-axios.defaults.headers("Access-Control-Allow-Origin", "*");
-
+// axios.defaults.headers = { "Access-Control-Allow-Origin": "*" };
 export const getSuggestions = async (value) => {
   return await axios.get(
     `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${value}`
